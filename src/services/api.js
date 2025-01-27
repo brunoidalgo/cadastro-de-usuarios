7536ea7db0adb80a0d52eef67a05f65fd09494b9
brunoidalgo <idalgob99@gmail.com>
2025-01-27 06:00:14 -0300

    Insert to new baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     // Base URL da API (Back-end)
-    baseURL: "http://localhost:3000"
+    baseURL: "https://express-mongodb-api-nay0.onrender.com"
+    // http://localhost:3000
+    // https://express-mongodb-api-nay0.onrender.com
 })
 
 export default api;
